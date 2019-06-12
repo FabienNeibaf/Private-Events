9dde78ab95a6c04c35140cadaf4da6aa041113fa
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-06-12 10:33:47 +0100

    Build and migrate your Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_094726) do
+ActiveRecord::Schema.define(version: 2019_06_12_093254) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.date "event_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
