eb7a05ebe2a930a3c94e7620762502ad020ff5aa
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-06-12 13:25:22 +0100

    Event create and show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,29 @@
 class EventsController < ApplicationController
+  def new
+    @event = current_user.creator_events.build
+    # debugger
+    
+  end
+
   def create
-    @event = Event.new
+    @event = current_user.creator_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :creator_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/create'
-  get 'events/show'
-  get 'events/index'
   root 'application#index'
   get 'signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
   delete 'signout', to: 'sessions#delete'
   resources :users, only: [:new, :create, :show]
+  resources :events, only: [:index, :new, :create, :show]
 end
